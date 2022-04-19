0f1a6ff4965908b21f5d51f235d248ef2d5bd5d2
hinzle <bosque4d@hotmail.com>
2022-04-19 16:24:32 -0400

    _

--- utils/imports.py
+++ utils/imports.py
@@ -13,6 +13,10 @@ import math
 import numpy as np
 import pandas as pd
 from scipy import stats
+
+# sci-kit-learn modules
+from sklearnex import patch_sklearn
+patch_sklearn()
 from sklearn.cluster import KMeans
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest, RFE, f_regression
